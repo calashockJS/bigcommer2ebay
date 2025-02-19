19388057b44083d29ae14f11ac1da2c38e0ce9e6
Judhisthira Sahoo <judhisahooo@gmail.com>
2025-02-19 22:49:13 +0530

    fixing url routing

--- api/index.php
+++ api/index.php
@@ -6,6 +6,16 @@ require __DIR__ . '/../vendor/autoload.php';
 // Bootstrap the application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// Adjust the request URI for Vercel deployment
+if ($_SERVER['REQUEST_URI'] !== '/' && file_exists(__DIR__ . '/../public' . $_SERVER['REQUEST_URI'])) {
+    return false;
+}
+
+// If using api prefix routes, fix the request path to prevent double /api/api
+if (strpos($_SERVER['REQUEST_URI'], '/api/api/') === 0) {
+    $_SERVER['REQUEST_URI'] = substr_replace($_SERVER['REQUEST_URI'], '/api/', 0, 9);
+}
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
