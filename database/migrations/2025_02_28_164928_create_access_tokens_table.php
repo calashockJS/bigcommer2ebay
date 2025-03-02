1eb82791d7104ce99fc6201a984e58eee0cd9c88
Judhisthira Sahoo <judhisahooo@gmail.com>
2025-03-02 11:01:42 +0530

    change migration code

--- database/migrations/2025_02_28_164928_create_access_tokens_table.php
+++ database/migrations/2025_02_28_164928_create_access_tokens_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('access_token',600);
+            $table->text('access_token');
             $table->string('refresh_token',300)->nullable();
             $table->string('expires_at',50)->nullable();
             $table->timestamps();
