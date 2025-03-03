f1ba6f103ac0b964fa01eae5d644e12ab3a4ccd6
Judhisthira Sahoo <judhisahooo@gmail.com>
2025-03-03 23:27:42 +0530

    chanage the sandbox bigcommerce configuration from client bigcommerce site

--- app/Http/Controllers/api/ApiController.php
+++ app/Http/Controllers/api/ApiController.php
@@ -17,14 +17,14 @@ use Illuminate\Support\Facades\Storage;
 
 class ApiController extends Controller
 {
-    //private $baseUrl = 'https://api.bigcommerce.com/stores/u4thb/v3';
-    private $baseUrl = 'https://api.bigcommerce.com/stores/l8m8i2ai7x/v3';
+    private $baseUrl = 'https://api.bigcommerce.com/stores/u4thb/v3';
+    //private $baseUrl = 'https://api.bigcommerce.com/stores/l8m8i2ai7x/v3';
 
     private $tokenFile = '';
 
     private $bigCommerceHeaders = [
-        //'X-Auth-Token' => '8solk9a5bhgkv19z7529lelq3c6mw24',
-        'X-Auth-Token' => '6tlsgp8ihjtquecp9lyyhqd58hpadez',
+        'X-Auth-Token' => '8solk9a5bhgkv19z7529lelq3c6mw24',
+        //'X-Auth-Token' => '6tlsgp8ihjtquecp9lyyhqd58hpadez',
         'Content-Type' => 'application/json',
     ];
 
@@ -1238,6 +1238,8 @@ class ApiController extends Controller
                 'error' => 'Invalid header value'
             ], 401);
         }
+
+        //$this->createEbayProductWithBCSku($bcsku);
 
         if (Storage::exists($filePath)) {
             $jsonData = json_decode(Storage::get($filePath), true);
