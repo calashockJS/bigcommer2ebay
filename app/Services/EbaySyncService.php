8614a9d973b9ba5babb44fd2446210035f2aac95
Judhisthira Sahoo <judhisahooo@gmail.com>
2025-03-06 15:17:52 +0530

    remove some more big data log

--- app/Services/EbaySyncService.php
+++ app/Services/EbaySyncService.php
@@ -60,17 +60,17 @@ class EbaySyncService
         }
         Log::channel('stderr')->info('now in EbaySyncService now in constructure');
         $ebayAccessToken = $this->accessToken;
-        Log::channel('stderr')->info( '$ebayAccessToken ::'.$ebayAccessToken);
+        Log::channel('stderr')->info('now in EbaySyncService now in constructure $ebayAccessToken ::');//.$ebayAccessToken);
         if($ebayAccessToken==''){
             Log::channel('stderr')->info('now in EbaySyncService now calling getUpdateAccessToken() due to $ebayAccessToken is empty');
             $ebayAccessToken = $this->getUpdateAccessTokenService($ebayAccessToken);
-            Log::channel('stderr')->info('now in EbaySyncService got $ebayAccessToken ::'.$ebayAccessToken.' from $this->getUpdateAccessTokenService($ebayAccessToken)');
+            Log::channel('stderr')->info('now in EbaySyncService got $ebayAccessToken ::++++++++++++++ from $this->getUpdateAccessTokenService($ebayAccessToken)');
             $this->accessToken = $ebayAccessToken;
         }else{
-            Log::channel('stderr')->info('now in EbaySyncService in constructure $ebayAccessToken is not empty $ebayAccessToken and $this->accessToken:: '.$ebayAccessToken.'   @@@@@@ '.$this->accessToken);
+            Log::channel('stderr')->info('now in EbaySyncService in constructure $ebayAccessToken is not empty $ebayAccessToken and $this->accessToken:: ');//.$ebayAccessToken.'   @@@@@@ '.$this->accessToken);
         }
         
-        Log::channel('stderr')->info('now in EbaySyncService now at end of  EbaySyncService consutructur method :: $this->accessToken ::'.$this->accessToken);
+        Log::channel('stderr')->info('now in EbaySyncService now at end of  EbaySyncService consutructur method :: $this->accessToken ::');//.$this->accessToken);
     }
 
     public function syncProductToEbay($sku)
